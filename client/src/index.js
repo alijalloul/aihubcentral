3076989da4897362bf2b9d9c80482909cb132193
unknown <ali.z.jalloul@gmail.com>
2023-04-15 23:17:19 +0300

    removed ./env2

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log(process.env);
+console.log(process.env.TEST);
 root.render(
   <React.StrictMode>
     <App />
