dc923207edf96a8d1a9de447d7a0b5216b7644af
unknown <ali.z.jalloul@gmail.com>
2023-04-20 13:36:45 +0300

    ssr

--- client/server/server.js
+++ client/server/server.js
@@ -6,47 +6,22 @@ import ReactDOMServer from 'react-dom/server';
 import { Provider } from 'react-redux';
 import Store from '../src/redux/Store.js';
 import App from '../src/App.js';
-import { JSDOM } from 'jsdom';
 
 const PORT = 3000;
 const app = express();
 
-const router = express.Router();
+app.get(express.static(path.resolve('./build/index.html')));
 
-const serverRenderer = (req, res, next) => {
+app.get("/*", (req, res, next) => {
   fs.readFile(path.resolve('./build/index.html'), 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       return res.status(500).send('An error occurred');
     }
 
-    // Create a virtual DOM environment using jsdom
-    const dom = new JSDOM(data);
-    const document = dom.window.document;
-
-    // Render the React application
-    const appHtml = ReactDOMServer.renderToString(
-      <Provider store={Store}>
-        <App />
-      </Provider>
-    );
-
-    // Update the inner HTML of the root div in the HTML template
-    document.getElementById('root').innerHTML = appHtml;
-
-    // Send the updated HTML page as the response
-    return res.send(dom.serialize());
+    return res.send(data.replace('<div id="root"></div>',`<div id="root">${<Provider store={Store}><App /></Provider>}</div>`));
   });
-};
-
-// Catch-all route
-router.use('/*', serverRenderer);
-
-// Serve static files from the build directory
-router.use(express.static(path.resolve(__dirname, '..', 'build'), { maxAge: '30d' }));
-
-// Tell the app to use the above rules
-app.use(router);
+})
 
 app.listen(PORT, () => {
   console.log(`SSR running on port ${PORT}`);
