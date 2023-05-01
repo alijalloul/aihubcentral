bc9bf7eff4766ce4ff72bb477aa314b6ace71108
unknown <ali.z.jalloul@gmail.com>
2023-05-01 23:20:06 +0300

    uncommented the code editor

--- client/src/components/CodeEditor/CodeEditor.js
+++ client/src/components/CodeEditor/CodeEditor.js
@@ -7,23 +7,17 @@ import "./CodeEditor.css";
 export const CodeEditor = (props) => {
   const editorRef = useRef(null);
 
-  // useEffect(() => {
-  //   const view = new EditorView({
-  //     doc: props.code,
-  //     extensions: [basicSetup, javascript()],
-  //     parent: editorRef.current,
-  //   });
+  useEffect(() => {
+    const view = new EditorView({
+      doc: props.code,
+      extensions: [basicSetup, javascript()],
+      parent: editorRef.current,
+    });
 
-  //   return () => {
-  //     view.destroy();
-  //   };
-  // }, [props.code]);
+    return () => {
+      view.destroy();
+    };
+  }, [props.code]);
 
-  EditorView({
-    doc: props.code,
-    extensions: [basicSetup, javascript()],
-    parent: editorRef.current,
-  });
-
-  return <div className="bg-gray-900" ref={editorRef} />;
+  return <div className="bg-gray-900" />;
 };
