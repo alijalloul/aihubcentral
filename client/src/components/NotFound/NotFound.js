87e0dcb34f5a7f6386f429824017993e6e88e272
unknown <ali.z.jalloul@gmail.com>
2023-05-07 21:15:56 +0300

    phone styling

--- client/src/components/NotFound/NotFound.js
+++ client/src/components/NotFound/NotFound.js
@@ -8,7 +8,7 @@ const NotFound = () => {
         <div className="relative w-[60%] flex flex-col justify-center items-center text-center sm:w-[90%]">
             <h3 className={`text-8xl pb-5 mb-5 border-b-2 border-white ${isHovered ? "opacity-0" : "opacity-1"} transition-all ease-in-out duration-500 sm:text-5xl`}>There Is Nothing To Be Seen Here</h3>
             <h3 className={`text-7xl mb-5 ${isHovered ? "opacity-0" : "opacity-1"} transition-all ease-in-out duration-500 sm:text-4xl`}>BEGONE!!!</h3>
-            <Link to="/" onMouseOver={() => {setIsHovered(true)}} onMouseOut={() => {setIsHovered(false)}} className={`absolute flex justify-center items-center bottom-0 translate-y-[100%] px-20 py-5 border-4 border-white ${isHovered ? "rounded-[900px] w-[400px] h-[400px] translate-y-[10%] sm:w-[300px] sm:h-[300px]" : " rounded-[0px] w-[250px] h-[70px]"} active:bg-white active:text-black active:border-black transition-all ease-in-out duration-[1s]`}>Go Home</Link>
+            <Link to="/" onMouseOver={() => {setIsHovered(true)}} onMouseOut={() => {setIsHovered(false)}} className={`absolute flex justify-center items-center bottom-0 px-20 py-5 border-4 border-white ${isHovered ? "rounded-[900px] w-[400px] h-[400px] translate-y-[10%] sm:w-[300px] sm:h-[300px]" : "translate-y-[100%] rounded-[0px] w-[250px] h-[70px]"} active:bg-white active:text-black active:border-black transition-all ease-in-out duration-[1s]`}>Go Home</Link>
         </div>
     </div>
   )
